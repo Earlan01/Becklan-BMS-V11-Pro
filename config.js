0669b505e238f83e35820e908b3b080a1047549d
Earlan01 <earlan154@yahoo.com>
2026-08-25 16:51:14 +0000

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.BECKLAN_CONFIG = Object.freeze({
-  VERSION: "12.0",
-  APP_NAME: "Becklan BMS V12.0",
-  BMS_URL: "https://script.google.com/macros/s/AKfycbyieXVejLAvcMnJEsRpD3OPRuB3FgFKW_vcDmFNkU3vsTJOa-7s1IQNhHhD5wDiGb78/exec",
-  AUTO_OPEN: false
-});
